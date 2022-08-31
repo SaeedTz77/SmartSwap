3bff7a65b3ebe1b56679c1c1d6433715675830be
SaeedTz77 <112203302+SaeedTz77@users.noreply.github.com>
2022-09-01 01:52:34 +0430

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -4143,7 +4143,7 @@ export default class Home extends PureComponent {
                         
                         
                          <div
-                          className="ssBTNbar01 wow fadeInUp mt-14 mb-13" data-wow-delay="0.2s"
+                          className="ssBTNbar01 wow fadeInUp mt-1 mb-1" data-wow-delay="0.2s"
                           style={{ justifyContent: "center" }}
                         >
                           <Link to="/ownLicence" className="ssBtn01">
